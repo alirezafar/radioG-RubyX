ad60f0890403c6924698912d52b2a57cca9b10de
Alireza Farshbaf <alireza.farshbaf@gmail.com>
2015-09-11 11:20:12 -0700

    Image uploader improved

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,12 @@ class Song < ActiveRecord::Base
 	validates :artist_id, presence: true
 	validates :filename, presence: true
 	mount_uploader :artworkurl, PictureUploader
+	validate :artworkurl_size
+
+	private
+	def artworkurl_size
+		if artworkurl.size > 5.megabytes
+		errors.add(:artworkurl, "Should be smaller than 5Mb")
+		end
+	end
 end
